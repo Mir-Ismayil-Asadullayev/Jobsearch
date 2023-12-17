7b1296033a17fee4433d4f8e220ba7d8efc07962
Mir-Ismayil-Asadullayev <asadullayev.mir.ismayil@gmail.com>
2023-12-18 00:46:57 +0400

    Started 'Jobsearch' final project and created initial folder structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './assets/scss/_base/App.scss';
+
+function App() {
+
+  return (
+    <>
+      <h1 >Helllo</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'normalize.css';
+import App from './App';
+import './assets/scss/_base/index.scss';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
